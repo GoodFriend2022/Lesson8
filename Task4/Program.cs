e5d8dc6fbcb307beb77b5e52a20d7fa719b6e4d9
Uri_Klevcov <a-08-137.mail.ru>
2022-11-24 17:38:47 +0500

    Task4 fill matrix spiral

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -4,3 +4,50 @@
 // 12 13 14 05
 // 11 16 15 06
 // 10 09 08 07
+
+void FillSpiral(int[,] matrix)
+{
+    int count = 1;
+    for (int i = 0; i < matrix.GetLength(0) / 2; i++)
+    {
+        int j = i;
+        for (j = i; j < matrix.GetLength(1) - i; j++)
+        {
+            matrix[i, j] = count;
+            count++;
+        }
+        int k = i + 1;
+        for (k = i + 1; k < matrix.GetLength(0) - i; k++)
+        {
+            matrix[k, j - 1] = count;
+            count++;
+        }
+        for (j -= 2; j >= i; j--)
+        {
+            matrix[k - 1, j] = count;
+            count++;
+        }
+        for (k -= 2; k > i; k--)
+        {
+            matrix[k, i] = count;
+            count++;
+        }
+    }
+    if (matrix.GetLength(1) % 2 != 0) matrix[matrix.GetLength(1) / 2, matrix.GetLength(0) / 2] = count;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] numbers = new int[4, 4];
+FillSpiral(numbers);
+PrintMatrix(numbers);
